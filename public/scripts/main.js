5064e307117d49e386ff02b4c1de46d3af73fff0
eapldev <eduardoaparecido.dev@gmail.com>
2024-02-08 10:11:29 -0300

    Adicionando função copiar id no botão da sala

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,4 @@
-import  Modal  from './modal.js'
+import Modal from './modal.js'
 
 const modal = Modal()
 
@@ -6,14 +6,12 @@ const modalTitle = document.querySelector('.modal h2')
 const modalDescription = document.querySelector('.modal p')
 const modalButton = document.querySelector('.modal button')
 
-// Pegar todos os botões que existe com a classe check
-
 const checkButtons = document.querySelectorAll('.actions a.check')
 
-    checkButtons.forEach(button => {
-    
-        button.addEventListener("click", handleClick)
-            
+checkButtons.forEach(button => {
+
+    button.addEventListener("click", handleClick)
+
 })
 
 
@@ -24,20 +22,39 @@ deleteButton.forEach(button => {
 })
 
 
-function handleClick(event, check = true){
+function handleClick(event, check = true) {
     event.preventDefault()
     const text = check ? "Marcar como lida" : "Excluir"
     const slug = check ? "check" : "delete"
     const roomId = document.querySelector("#room-id").dataset.id
     const questionId = event.target.dataset.id
-   
+
     const form = document.querySelector(".modal form")
     form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
-    
+
     modalTitle.innerHTML = `${text} esta pergunta`
     modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
-    modalButton.innerHTML =  `Sim, ${text.toLocaleLowerCase()}`
-    check? modalButton.classList.remove("red") : modalButton.classList.add("red")
+    modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
 
     modal.open()
 }
+
+var botaoCopiar = document.getElementById('room-id');
+
+botaoCopiar.addEventListener('click', function () {
+    var textoParaCopiar = this.dataset.id;
+    var inputTemporario = document.createElement('input');
+    inputTemporario.setAttribute('value', textoParaCopiar);
+    document.body.appendChild(inputTemporario);
+    inputTemporario.select();
+
+    try {
+        var copiado = document.execCommand('copy');
+        var mensagem = copiado ? 'Texto copiado!' : 'Não foi possível copiar o texto.';
+        console.log(mensagem);
+    } catch (erro) {
+        console.error('Erro ao copiar o texto: ', erro);
+    }
+    document.body.removeChild(inputTemporario);
+});
